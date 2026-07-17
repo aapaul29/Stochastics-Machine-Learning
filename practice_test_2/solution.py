0ea84b673f3b5e573689cc246a24920cc6fb7d63
aapaul29 <aapaul@ethz.ch>
2026-07-17 15:50:58 +0200

    Done with stochastics problems

--- practice_test_2/solution.py
+++ practice_test_2/solution.py
@@ -1,24 +1,40 @@
 import math, scipy.stats
 
 def mean(samples: list) -> float:
-    return scipy.stats.mean(samples)
+    return sum(samples) / len(samples)
 
 def std_dev(samples: list) -> float:
-    return scipy.stats.std(samples)
+    n = len(samples)
+    sum = 0
+    coefficient = 1 / (n - 1)
+    samples_mean = mean(samples)
+    for x in samples:
+        sum += (x - samples_mean) ** 2
+    var = coefficient * sum
+    std = math.sqrt(var)
+    return std
 
 def t_statistic(samples: list[float], mu0: float) -> float:
-    """Return t = (xbar - mu0) / (s / sqrt(n)), rounded to 4 dp."""
-    # TODO
     n = len(samples)
-    t = (mean(samples) - mu0) / std_dev(samples) / math.sqrt(n)
+    s = std_dev(samples)
+    if s == 0:
+        xbar = mean(samples)
+        return round(math.inf if xbar > mu0 else (-math.inf if xbar < mu0 else 0.0), 4)
+    t = (mean(samples) - mu0) / (s / math.sqrt(n))
     return round(t, 4)
+
 
 def p_value(samples: list[float], mu0: float) -> float:
     """Return two-sided p-value for one-sample t-test, rounded to 4 dp."""
     # TODO
-    return -1.0
+    n = len(samples)
+    t = t_statistic(samples, mu0)
+    p = 2 * (1 - scipy.stats.t.cdf(abs(t), df=n-1))
+    return round(p, 4)
 
 def reject_null(samples: list[float], mu0: float, alpha: float) -> bool:
     """Return True if p-value < alpha."""
     # TODO
-    return False
+    p = p_value(samples, mu0)
+    return p < alpha
+    
--- practice_test_3/solution.py
+++ practice_test_3/solution.py
@@ -1,16 +1,47 @@
 import math, scipy.stats
 
+def mean(samples: list[float]) -> float:
+    return sum(samples) / len(samples)
+
+def std_dev(samples):
+    n = len(samples)
+    coefficient  = 1 / (n - 1)
+    samples_mean = mean(samples)
+    sum = 0
+    for x in samples:
+        sum += (x - samples_mean) ** 2
+    var = coefficient * sum
+    std = math.sqrt(var)
+    return std
+
 def welch_t_statistic(xs: list[float], ys: list[float]) -> float:
     """Return Welch's t statistic, rounded to 4 dp."""
     # TODO
-    return -1.0
+    m = len(xs)
+    n = len(ys)
+    x_mean = mean(xs)
+    y_mean = mean(ys)
+    s_x = std_dev(xs)
+    s_y = std_dev(ys)
+    t = (x_mean - y_mean) / math.sqrt((s_x ** 2 / m) + (s_y ** 2 / n))
+    return round(t, 4)
 
 def welch_p_value(xs: list[float], ys: list[float]) -> float:
     """Return two-sided p-value for Welch's t-test, rounded to 4 dp."""
     # TODO
-    return -1.0
+    ## compute nu
+    m = len(xs)
+    n = len(ys)
+    s_x = std_dev(xs)
+    s_y = std_dev(ys)
+    nu = ((s_x ** 2 / m) + (s_y ** 2 / n)) ** 2 / ((s_x ** 2 / m) ** 2/(m - 1) + (s_y ** 2 / n) ** 2 / (n - 1))
+    
+    t = welch_t_statistic(xs, ys)
+    p = 2 * (1 - scipy.stats.t.cdf(abs(t), df=nu))
+    return round(p, 4)
 
 def reject_null(xs: list[float], ys: list[float], alpha: float) -> bool:
     """Return True if p-value < alpha."""
     # TODO
-    return False
+    p = welch_p_value(xs, ys)
+    return p < alpha
