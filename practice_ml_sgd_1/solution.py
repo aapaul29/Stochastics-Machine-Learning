e4ac43d3c4a55adc6d290f887d973c311aabcffb
aapaul29 <aapaul@ethz.ch>
2026-07-20 15:16:39 +0200

    sgd problems updated

--- practice_ml_sgd_1/solution.py
+++ practice_ml_sgd_1/solution.py
@@ -1,6 +1,34 @@
+def inner_product(w : list[float], x: list[float]) -> float:
+    ip = 0
+    for i in range(len(w)):
+        ip += w[i] * x[i]
+    return ip
+    
+
+
+def w_step(w: list[float], x: list[float], b: float, alpha: float, y: float, i: float) -> float:
+    gradient = 2 * (inner_product(w, x) + b - y) * x[i]
+    w_i = w[i] - alpha * gradient
+    return round(w_i, 6)
+
+
+def b_step(w: list[float], x: list[float], b: float, alpha: float, y: float) -> float:
+    gradient = 2 * (inner_product(w, x) + b - y)
+    b = b - alpha * gradient
+    return round(b, 6)
+
+
+
 def sgd_update(w: list[float], b: float, x: list[float],
                y: float, alpha: float) -> tuple[list[float], float]:
     """Return (w_new, b_new) after one SGD step on sample (x, y).
     Each value rounded to 6 decimal places."""
     # TODO
-    return w, b
+    w_new = []
+    b_new = b_step(w, x, b, alpha, y)
+    for i in range(len(w)):
+        w_i = w_step(w, x, b, alpha, y, i)
+        w_new.append(w_i)
+        
+
+    return (w_new, b_new)
--- practice_ml_sgd_2/solution.py
+++ practice_ml_sgd_2/solution.py
@@ -1,5 +1,18 @@
+def polynomial(w: list[float], x: float) -> float:
+    ip = 0
+    for i in range(len(w)):
+        ip += w[i] * x ** i
+    return ip
+
+
 def poly_sgd_update(w: list[float], x: float, y: float, alpha: float) -> list[float]:
     """Return updated polynomial coefficients after one SGD step.
     Each coefficient rounded to 6 decimal places."""
     # TODO
-    return w
+    w_new = []
+    for i in range(len(w)):
+        gradient = 2 * (polynomial(w, x) - y) * x ** i
+        w_i = w[i] - alpha * gradient
+        w_new.append(w_i)
+
+    return w_new
